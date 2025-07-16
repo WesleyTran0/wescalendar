7701b1f6e0e97e286cb0ecb026038933efcd6148
WesleyTran0 <wesley.tran24@gmail.com>
2025-07-16 04:50:08 -0400

    Added testing Post request

--- src/components/calendar/DayCell.tsx
+++ src/components/calendar/DayCell.tsx
@@ -6,11 +6,31 @@ type DayCellProps = {
   onClick?: () => void;
 };
 
+const createTask = async () => {
+  try {
+    const response = await fetch("http://localhost:8080/api/events/test", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(""),
+    });
+
+    const result = await response.json();
+    console.log("Event returned: ", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
 export default function DayCell({ day, hour }: DayCellProps) {
   return (
     <div
       className={"h-20 border border-bg-highlight"}
-      onClick={() => console.log(day.toDateString())}
-    ></div>
+      // onClick={() => console.log(day.toDateString())}
+      onClick={createTask}
+    >
+      test create task connection
+    </div>
   );
 }
